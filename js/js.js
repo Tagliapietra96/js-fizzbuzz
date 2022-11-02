207253388ad733523eb48c23bc2626b3a2c3bfc0
Tagliapietra96 <tagliatek96@gmail.com>
2022-11-02 13:47:40 +0100

    completed

---
+++ js/js.js
@@ -0,0 +1,15 @@
+const rowEl = document.querySelector('.d-flex');
+
+for (i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        rowEl.innerHTML += `<div class="my-col bg-danger">FizzBuzz</div>`;
+    } else if (i % 3 === 0) {
+        rowEl.innerHTML += `<div class="my-col bg-success">Fizz</div>`;
+    } else if (i % 5 === 0) {
+        rowEl.innerHTML += `<div class="my-col bg-warning">Buzz</div>`;
+    } else {
+        rowEl.innerHTML += `<div class="my-col bg-light">${i}</div>`;
+    };
+
+};
